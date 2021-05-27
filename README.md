ae81f685b3398caf5a488aef8b887be39566b15e
hanjisoo95 <gkswltn66@naver.com>
2021-05-28 02:57:47 +0900

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Test OSS-Team-9
---
+++ src/Window.java
@@ -0,0 +1,41 @@
+
+public class Window {
+	private String programName;		// 프로그램 명
+	private String tabName;			// 탭 명
+	private String startTime;		// 시작 시간
+	
+	public Window() {
+		
+	}
+	
+	public Window(String programName, String tabName, String startTime) {
+		this.programName = programName;
+		this.tabName = tabName;
+		this.startTime = startTime;
+	}
+	
+	public String getProgramName() {
+		return programName;
+	}
+	
+	public void setProgramName(String programName) {
+		this.programName = programName;
+	}
+	
+	public String getTabName() {
+		return tabName;
+	}
+	
+	public void setTabName(String tabName) {
+		this.tabName = tabName;
+	}
+	
+	public String getStartTime() {
+		return startTime;
+	}
+	
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	
+}
